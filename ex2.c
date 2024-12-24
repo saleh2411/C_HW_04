05d72243a04914ffb2136b72700e0f8d1f8e554b
saleh2411 <salehkhalil1124@gmail.com>
2024-12-24 23:42:49 +0200

    imp perimeter

--- ex2.c
+++ ex2.c
@@ -15,8 +15,13 @@ typedef struct triangle {
 
 // Function to calculate the perimeter of a triangle in 3 D
 double perimeter(const Triangle* t) {
-    
+    double d1 = sqrt(pow(t->p.x - t->q.x, 2) + pow(t->p.y - t->q.y, 2) + pow(t->p.z - t->q.z, 2));
+    double d2 = sqrt(pow(t->q.x - t->r.x, 2) + pow(t->q.y - t->r.y, 2) + pow(t->q.z - t->r.z, 2));
+    double d3 = sqrt(pow(t->r.x - t->p.x, 2) + pow(t->r.y - t->p.y, 2) + pow(t->r.z - t->p.z, 2));
+    return d1 + d2 + d3;
 }
+    
+
 
 // Function to calculate the area of a triangle in 3D 
 double area(const Triangle* t) {
