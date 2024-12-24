85bd9f265453c76aec7276d0b7f6b0aa7dfa7dc5
saleh2411 <salehkhalil1124@gmail.com>
2024-12-24 23:16:12 +0200

    scanf imp

--- ex2.c
+++ ex2.c
@@ -23,7 +23,15 @@ double area(const Triangle* t) {
 }
 
 int main() {
+
+    double x, y, z;
+    for (int i = 1; i <= 3; i++)
+    {
+        printf("Point %i (x y z): ");
+        scanf("%lf %lf %lf", &x, &y, &z);
+    }
     
+
     // Print the results
     printf("Perimeter: %.6f\n", perimeter);
     printf("Area: %.6f\n", area);
